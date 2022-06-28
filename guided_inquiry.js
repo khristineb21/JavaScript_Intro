db295b43632bd6b0accec4a31ead6d0624593feb
Khristine Bautista <khristinebautista21@gmail.com>
2022-06-29 04:31:26 +0800

    started assignment

---
+++ guided_inquiry.js
@@ -0,0 +1,16 @@
+// Very Easy
+
+var num1 = 21;
+var num2 = 10;
+
+console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`)
+
+// Easy
+
+var name1 = "Jeremiah"
+var name2 = "Conrad"
+
+console.log(`The name ${name1} is longer than ${name2} by ${name1.length - name2.length} characters.`)
+
+// Medium
+
