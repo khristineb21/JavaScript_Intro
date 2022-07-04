a459f893354dc7d500aebea8960ae56182162662
Khristine Bautista <khristinebautista21@gmail.com>
2022-07-04 18:17:15 -0400

    finished all the challenges

--- guided_inquiry.js
+++ guided_inquiry.js
@@ -10,12 +10,23 @@ console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`)
 var name1 = "Jeremiah"
 var name2 = "Conrad"
 
-console.log(`The name ${name1} is longer than ${name2} by ${name1.length - name2.length} characters.`)
+if (name1.length > name2.length) {
+  console.log(`${name1}'s name is longer than ${name2}'s name by ${name1.length - name2.length} characters`)
+} else {
+  console.log(`${name2}'s name is longer than ${name1}'s name by ${name2.length - name1.length} characters`)
+}
 
-// // Medium
-// var name = window.prompt("Type Anything: ")
-// console.log(``)
+// Medium
 
+var string = prompt("Type Anything: ")
+
+if (string.toUpperCase() === string) {
+  console.log("You are shouting!");
+} else if (string.toLowerCase() === string) {
+  console.log("You are whispering.")
+} else {
+  console.log("You are talking normally.")
+}
 
 // Hard
 
@@ -26,3 +37,23 @@ console.log(x += y)
 console.log(x -= y)
 console.log(x *= y)
 console.log(x /= y)
+
+// Very Hard
+
+numberOne = parseFloat(prompt("Enter a number"))
+operator = prompt("Enter an operator")
+numberTwo = parseFloat(prompt("Enter another number"))
+
+let product;
+
+if (operator === "+") {
+  product = numberOne + numberTwo;
+} else if (operator === "-") {
+  product = numberOne - numberTwo;
+} else if (operator === "*") {
+  product = numberOne * numberTwo;
+} else {
+  product = numberOne / numberTwo;
+}
+
+console.log(`${numberOne} ${operator} ${numberTwo} = ${product}`)
